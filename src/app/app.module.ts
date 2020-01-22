bdcc01aea0969e1b571ce3efbc8d32fd6229304d
ElizabetDunayeva <sms-elizabet@mail.ru>
2020-01-22 23:51:53 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppChatComponent } from './app-chat/app-chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
