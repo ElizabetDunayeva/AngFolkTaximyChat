fa8026c92cfd350224ff6cd624e360b9b1693af2
ElizabetDunayeva <sms-elizabet@mail.ru>
2020-01-29 23:27:18 +0300

    new

--- src/app/app-chat/app-chat.component.ts
+++ src/app/app-chat/app-chat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Message} from "./model/message";
+import {Chat} from "./model/chat";
 
 @Component({
   selector: 'app-app-chat',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-chat.component.css']
 })
 export class AppChatComponent implements OnInit {
+  messages: Message[] = [];
+  chats: Chat[] =[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getAllMessages();
+  }
+  public getAllMessages(ChatId: number=1){
+    let url ="http://localhost:1337/Chat/chat" + ChatId;
+    this.http.get<Message[]>(url).subscribe(
+      res=> {
+        this.messages = res;
+      },
+      err => {
+        alert("An error has occured;")
+      }
+    );
+  }
+  public getAllChats(){
+    let url ="http://localhost:1337/Chat/all/" ;
+    this.http.get<Chat[]>(url).subscribe(
+      res=> {
+        this.chats = res;
+      },
+      err => {
+        alert("An error has occured;")
+      }
+    );
+
   }
 
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,38 @@ import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppChatComponent } from './app-chat/app-chat.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { NoteComponent } from './note/note.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes :Routes = [
+  {
+    path: 'Chats',
+    component: AppChatComponent
+  },
+  {
+    path:'',
+    component: AppChatComponent,
+    pathMatch:'full'
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppChatComponent
+    AppChatComponent,
+    NavigationComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    NgbModule
+    FormsModule,
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
